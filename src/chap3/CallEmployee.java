01bf2df5331035f3f80e959114b539d6b15c1c9e
papercut <papercut@172.17.60.213>
2019-08-13 11:59:18 +0700

    2 commit

---
+++ src/chap3/CallEmployee.java
@@ -0,0 +1,15 @@
+package chap3;
+
+public class CallEmployee {
+    public static void main(String[] args) {
+        //call static attr & method
+        System.out.println("Id :"+Employee.id);
+        Employee.work();
+        
+        //call non-static
+        Employee em = new Employee();
+        System.out.println("Name : "+em.name);       
+        System.out.println("Age : "+em.age);
+        em.getSalary(); 
+    }       
+}
